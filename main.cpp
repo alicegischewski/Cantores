491357955a9cdeef1ea66a34201bcbbd72bea350
alicegischewski <alice.nogueira1@estudante.ufla.br>
2026-05-26 20:10:31 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,23 @@
 #include <fstream>
 using namespace std;
 
+void menu(){
+	cout<<"1- Inserir novo registro."<<endl;
+	cout<<"2- Remover registro."<<endl;
+	cout<<"3- Buscar registro."<<endl;
+	cout<<"4- Ordenar dados."<<endl;
+	cout<<"5- Vizualizar dados"<<endl;
+	cout<<"6- Salvar alteracoes."<<endl;
+	cout<<"7- Sair"<<endl;
+	
+}
+
 int main(){
+	ifstream entrada("entrada.csv");
+	ofstream saida ("saida.csv");
 
 
   return 0;
 
 }
+
